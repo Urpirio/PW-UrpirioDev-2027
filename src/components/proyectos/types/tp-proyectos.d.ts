6411c394427eab7e06112c22e9ce3c85132db997
urpirio <urpiriojunior@gmail.com>
2026-07-18 22:41:20 -0400

    types: add proyectos page type definitions

---
+++ src/components/proyectos/types/tp-proyectos.d.ts
@@ -0,0 +1,16 @@
+export interface object_proyectos {
+  name: string;
+  description: string;
+  technology: [{ name: string }, { name: string }, { name: string }];
+  url_proyecto: string;
+  url_githud: string;
+  empresa: string;
+  images: [
+    {
+      url: string;
+      alt: string;
+    },
+  ];
+}
+
+export type tp_proyectos = object_proyectos[];
